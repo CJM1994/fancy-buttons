7a3f5c0ac7af48cb44b647422460049d5677be23
CJM1994 <connorjohnmullin@gmail.com>
2022-03-21 14:24:48 -0700

    Refactor lights

--- src/components/LightswitchButton.js
+++ src/components/LightswitchButton.js
@@ -3,15 +3,7 @@ import React, { useState } from "react";
 const LightswitchButton = () => {
 
   const [light, setLight] = useState(0);
-
-  const handleClick = () => {
-    if (light === 0) {
-      setLight(1);
-    }
-    else {
-      setLight(0);
-    };
-  };
+  const handleClick = () => { setLight(light ? 0 : 1) };
 
   return (
     <button onClick={handleClick} className="LightswitchButton">
