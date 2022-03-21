fb2cfe89719c7a0d1c990bd8d0febe91fb59e417
CJM1994 <connorjohnmullin@gmail.com>
2022-03-21 14:07:05 -0700

    Add listener counter

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 
 const CounterButton = () => {
 
-  const [clickAmount, setClickAmount] = useState();
+  const [clickAmount, setClickAmount] = useState(0);
+
+  const handleClick = () => {
+    setClickAmount(clickAmount + 1);
+  };
 
   return (
-    <button className="CounterButton">
-      You clicked me X amount of times
+    <button onClick={handleClick} className="CounterButton">
+      You clicked me {clickAmount} times
     </button>
   );
 
